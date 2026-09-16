c0c2f86b3c278506e5e5740bb74c8cefa044e137
James Harrison <james090500@gmail.com>
2026-09-16 19:52:59 +0100

    Update Gradle and fix user count

--- src/main/java/net/minecraftcapes/listeners/MotdListener.java
+++ src/main/java/net/minecraftcapes/listeners/MotdListener.java
@@ -8,21 +8,26 @@ import net.kyori.adventure.text.minimessage.MiniMessage;
 import net.kyori.adventure.text.minimessage.tag.resolver.Placeholder;
 import net.minecraftcapes.MinecraftCapesAuth;
 import net.minecraftcapes.configs.Configs;
+import net.minecraftcapes.utils.WebUtils;
 
 public class MotdListener {
 
 	@Subscribe
 	public void onPing(ProxyPingEvent event) {
+		WebUtils.UsersResponse usersResponse = MinecraftCapesAuth.getInstance().getUsersResponse();
 		if(
-				MinecraftCapesAuth.getInstance().getUsersResponse() != null &&
-				MinecraftCapesAuth.getInstance().getUsersResponse().success()
+				usersResponse != null &&
+						usersResponse.success()
 		) {
 			MiniMessage miniMessage = MiniMessage.miniMessage();
+
+			// Pretty User Count
+			String users = String.format("%,d", Integer.parseInt(usersResponse.users()));
 
 			// Apply placeholders
 			Component finalMessage = miniMessage.deserialize(
 					Configs.settings.MOTD,
-					Placeholder.parsed("users", MinecraftCapesAuth.getInstance().getUsersResponse().users())
+					Placeholder.parsed("users", users)
 			);
 
 			ServerPing ping = event.getPing().asBuilder().description(finalMessage).build();
